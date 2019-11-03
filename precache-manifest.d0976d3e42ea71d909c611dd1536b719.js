1bf23d9a91b34e53909975877d6b76607f34e0aa
Cleanderson Lins <cleandersonlins@gmail.com>
2019-11-03 09:18:04 -0300

    Updates

---
+++ precache-manifest.d0976d3e42ea71d909c611dd1536b719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a7eeec7a0a0d19a1b146187316a0bf",
+    "url": "/kleanlins/me/index.html"
+  },
+  {
+    "revision": "d1126c1ec4b8d7599344",
+    "url": "/kleanlins/me/static/css/main.ca17b451.chunk.css"
+  },
+  {
+    "revision": "16bf027f5d9e728eec01",
+    "url": "/kleanlins/me/static/js/2.114b55ff.chunk.js"
+  },
+  {
+    "revision": "d1126c1ec4b8d7599344",
+    "url": "/kleanlins/me/static/js/main.dd3898aa.chunk.js"
+  },
+  {
+    "revision": "364cd7f3d26024e234cf",
+    "url": "/kleanlins/me/static/js/runtime-main.20c73931.js"
+  }
+]);
