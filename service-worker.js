70da172a813a240fb50b917e26cb82d78813e581
Cleanderson Lins <cleandersonlins@gmail.com>
2019-11-06 01:05:47 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d6231566001b581e814a3fe61a84343.js"
+  "/precache-manifest.b694f349bf4f1cf84288906691198711.js"
 );
 
 self.addEventListener('message', (event) => {
